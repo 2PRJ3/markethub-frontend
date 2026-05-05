8fd213be44d7e9f8646b4e1ecd1a9fe6a5b686d3
yelasad <elasadbennordinehamissi@gmail.com>
2026-05-05 15:59:42 +0200

    feat(page): Add first section of homepage with the search bar

---
+++ src/components/TheSearchBar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import InputText from 'primevue/inputtext'
+import IconField from 'primevue/iconfield'
+import InputIcon from 'primevue/inputicon'
+import Button from 'primevue/button'
+</script>
+<template>
+  <div class="flex items-center justify-center w-md gap-3">
+    <IconField class="flex-1 ">
+      <InputIcon class="pi pi-search" />
+      <InputText
+        type="search"
+        placeholder="Rechercher un service (ex: Design, Tutorat, Rédaction...)"
+        class="w-full rounded-lg focus:outline-none truncate"
+        variant="outlined"
+      />
+    </IconField>
+    <Button type="button" label="Chercher" class="bg-[#3525CD] w-max" />
+  </div>
+</template>
