4ca6f686b1f2805f3583559ae8b2eb129d035de0
yelasad <elasadbennordinehamissi@gmail.com>
2026-05-05 15:00:24 +0200

    feat(navbar): Add navbar design

---
+++ src/types/nav.ts
@@ -0,0 +1,10 @@
+import type { MenuItem} from 'primevue/menuitem'
+
+
+export interface NavItem extends MenuItem {
+  label?: string
+  route?: string
+  icon?: string
+  class?: string
+  separator?: boolean
+}
