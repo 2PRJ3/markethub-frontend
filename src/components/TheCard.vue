d2aec12b8d64802a8c65ec93526f43c4db1a9be3
yelasad <elasadbennordinehamissi@gmail.com>
2026-05-06 11:51:18 +0200

    feat(page): Add sections services

---
+++ src/components/TheCard.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import Card from 'primevue/card'
+import Tag from 'primevue/tag'
+import Avatar from 'primevue/avatar'
+import Rating from 'primevue/rating'
+
+import imagedefault from '@/assets/pictures/Background.png'
+import avatar from '@/assets/pictures/avatar.png'
+</script>
+
+<template>
+  <Card style="width: max-content; overflow: hidden">
+    <template #header>
+      <div class="relative">
+        <img :src="imagedefault" alt="logo" />
+        <Tag value="Design" severity="info" class="absolute" style="left: 5px; top: 5px" />
+      </div>
+    </template>
+    <template #title>
+      <div class="flex items-center gap-3">
+        <Avatar :image="avatar" shape="circle" />
+        <span class="text-sm">Thomas D.</span>
+      </div>
+    </template>
+
+    <template #content>
+      <div>
+        <span class="font-semibold text-gray-900 leading-tight tracking-tigh"
+          >Design de logos minimalistes</span
+        >
+      </div>
+    </template>
+
+    <template #footer>
+      <div class="flex justify-between items-center gap-3">
+        <div class="flex items-center gap-2">
+          <Rating
+            :stars="1"
+            :pt="{
+              onIcon: { class: 'pi pi-star-fill !text-yellow-400 hover:border-none' },
+              offIcon: { class: 'pi pi-star-fill !text-yellow-400 hover:border-none' },
+            }"
+          />
+          <span>4.9</span>
+        </div>
+
+        <div>
+          <span class="text-gray-950">Dés 15 €</span>
+        </div>
+      </div>
+    </template>
+  </Card>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,31 @@
 <script setup lang="ts">
 import TheSearchBar from '@/components/TheSearchBar.vue'
+import TheCard from '@/components/TheCard.vue'
 </script>
 
 <template>
+  <section class="flex flex-col items-center justify-center gap-5 px-4 py-20">
+    <h1
+      class="text-center text-2xl font-bold text-gray-900 sm:text-5xl md:text-3xl lg:text-6xl max-w-4xl leading-tight tracking-tight"
+    >
+      Trouvez les services parfaits pour vos projets étudiants
+    </h1>
+    <TheSearchBar />
+  </section>
 
-    <section class="flex flex-col items-center justify-center gap-5 px-4 py-20">
-      <h1
-        class="text-center text-2xl font-bold text-gray-900 sm:text-5xl md:text-3xl lg:text-6xl max-w-4xl leading-tight tracking-tight"
-      >
-        Trouvez les services parfaits pour vos projets étudiants
-      </h1>
-      <TheSearchBar/>
-    </section>
-
-
-
+  <section class="flex flex-col gap-5 px-8 pr-8">
+    <div class="flex flex-col">
+      <h2 class="font-bold text-2xl">Nos services</h2>
+      <div class="flex justify-between">
+        <span class="text-[#464555]">Découvrez ce que la communauté vous propose.</span>
+        <router-link to="/" class="hidden md:flex md:items-end gap-1 text-[#3525CD]">
+          <span class="text-sm">Voir tout</span>
+          <i class="pi pi-arrow-right" />
+        </router-link>
+      </div>
+    </div>
+    <div class="flex items-center md:justify-start justify-center py-5">
+      <TheCard />
+    </div>
+  </section>
 </template>
